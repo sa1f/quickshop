8719a372579ea6cdcc08e03d789132be960f912e
Benjamin <benjaminlang12@gmail.com>
2018-04-03 16:56:09 -0700

    new and improved python script

--- python/host.py
+++ python/host.py
@@ -10,9 +10,6 @@ def main(args):
 	ser = serial.Serial('/dev/tty.usbserial', 115200)
 	ser.flush()
 
-	
-
-
 	while True:
 		r = requests.get('http://saif.ms:3000/needs_hashing').json();
 		# check if there is a valid instance to hash
@@ -20,31 +17,28 @@ def main(args):
 			time.sleep(0.5)
 		else:	
 			message = "$"
-			message.append(r.get('block'))
-			message.append(r.get('data'))
-			message.append(r.get('prev_hash' + '$'))
+			message = message + str(r.get('block'))
+			message = message + r.get('data')
+			message = message + r.get('prev_hash') + '$'
 		
-			for i in range(len(msgString)):
-				ser.write(msgString[i].encode())
+			for i in range(len(message)):
+				ser.write(message[i].encode())
 				# wait for 1 milisecond because python is dumb
 				# and sends data faster than the actual buadrate... wtf
 				time.sleep(0.001)  
 			ser.flush()
 
 			time.sleep(1./120)
-			hashData = str(ser.readline().hex())
-			nonce = str(ser.readline().hex())
+			hashData = str(ser.readline().hex())[:-4]
+			time.sleep(1./120)
+			nonce = str(ser.readline().hex())[:-4]
 
-			p = requests.post('http://saif.ms:3000/register_hash', data = {'curr_hash': hashData, 'nonce': nonce})
+			p = requests.post('http://saif.ms:3000/register_hash', 
+				data = {'curr_hash': hashData, 'nonce': int(nonce, 16)})
 			print(p.json())
 			break
 			
 	ser.close()
-	
-	
-
-	
-
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
