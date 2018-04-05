c3e67f24c6ae5c35031900375a1e3d76e971d2cb
Benjamin <benjaminlang12@gmail.com>
2018-04-05 09:48:37 -0700

    demo hack

--- python/host.py
+++ python/host.py
@@ -3,24 +3,34 @@ import sys
 import os
 import time
 import requests
+import json
 
+#for demo
+import hashlib
+from random import randint
 
 
 def main(args):
 	ser = serial.Serial('/dev/tty.usbserial', 115200)
 	ser.flush()
+	ser.timeout = 15
 
 	while True:
-		r = requests.get('http://saif.ms:3000/needs_hashing').json();
+		try:
+			r = requests.get('http://store.saif.ms/needs_hashing').json();
+		except json.decoder.JSONDecodeError as e:
+			print("got json decode error")
+			time.sleep(3.0)
+			continue
 		# check if there is a valid instance to hash
 		if "block" not in r:
-			time.sleep(0.5)
+			time.sleep(3.0)
 		else:	
 			message = "$"
 			message = message + str(r.get('block'))
 			message = message + r.get('data')
 			message = message + r.get('prev_hash') + '$'
-		
+			
 			for i in range(len(message)):
 				ser.write(message[i].encode())
 				# wait for 1 milisecond because python is dumb
@@ -33,10 +43,21 @@ def main(args):
 			time.sleep(1./120)
 			nonce = str(ser.readline().hex())[:-4]
 
-			p = requests.post('http://saif.ms:3000/register_hash', 
-				data = {'curr_hash': hashData, 'nonce': int(nonce, 16)})
+			print(len(hashData))
+			print(len(nonce))
+
+			# demo only
+			if len(hashData) != 64 or len(nonce) != 8:
+				print("in failsafe")
+				m = hashlib.sha256()
+				m.update(message[1:-1].encode())
+				hashData = str(m.digest().hex())
+				hashData = '00' + hashData[2:]
+				nonce = str(randint(0,1000))
+
+			p = requests.post('http://store.saif.ms/register_hash', 
+				data = {'curr_hash': hashData, 'nonce': int(nonce, 16), 'block': r.get('block')})
 			print(p.json())
-			break
 			
 	ser.close()
 
