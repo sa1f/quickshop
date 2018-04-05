5ef8b5ee0f7c922720260ce1f6d338a5c8b75a76
Benjamin <benjaminlang12@gmail.com>
2018-04-05 16:34:13 -0700

    removed dead code from module 1

--- de1/design/qsys/pwm_pll/design/pwm_pll_inst.v
+++
@@ -1,8 +0,0 @@
-	pwm_pll u0 (
-		.clk_in_clk     (<connected-to-clk_in_clk>),     //   clk_in.clk
-		.reset_in_reset (<connected-to-reset_in_reset>), // reset_in.reset
-		.pwm_clk0_clk   (<connected-to-pwm_clk0_clk>),   // pwm_clk0.clk
-		.pwm_clk1_clk   (<connected-to-pwm_clk1_clk>),   // pwm_clk1.clk
-		.pwm_clk2_clk   (<connected-to-pwm_clk2_clk>)    // pwm_clk2.clk
-	);
-
