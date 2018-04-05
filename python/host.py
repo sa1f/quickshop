18f69ea12c1f30beee6e7ccb6b2ed7f6311adf23
Benjamin <benjaminlang12@gmail.com>
2018-04-05 15:54:56 -0700

    removed demo extra code

--- python/host.py
+++ python/host.py
@@ -5,10 +5,6 @@ import time
 import requests
 import json
 
-#for demo
-import hashlib
-from random import randint
-
 
 def main(args):
 	ser = serial.Serial('/dev/tty.usbserial', 115200)
@@ -28,13 +24,14 @@ def main(args):
 		else:	
 			message = "$"
 			message = message + str(r.get('block'))
-			message = message + r.get('data')
-			message = message + r.get('prev_hash') + '$'
+			if 'data' in r and r.get('data') is not None:
+				message = message + r.get('data')
+			if 'prev_hash' in r and r.get('prev_hash') is not None:
+				message = message + r.get('prev_hash')
+			message = message + '$'
 			
 			for i in range(len(message)):
 				ser.write(message[i].encode())
-				# wait for 1 milisecond because python is dumb
-				# and sends data faster than the actual buadrate... wtf
 				time.sleep(0.001)  
 			ser.flush()
 
@@ -42,18 +39,6 @@ def main(args):
 			hashData = str(ser.readline().hex())[:-4]
 			time.sleep(1./120)
 			nonce = str(ser.readline().hex())[:-4]
-
-			print(len(hashData))
-			print(len(nonce))
-
-			# demo only
-			if len(hashData) != 64 or len(nonce) != 8:
-				print("in failsafe")
-				m = hashlib.sha256()
-				m.update(message[1:-1].encode())
-				hashData = str(m.digest().hex())
-				hashData = '00' + hashData[2:]
-				nonce = str(randint(0,1000))
 
 			p = requests.post('http://store.saif.ms/register_hash', 
 				data = {'curr_hash': hashData, 'nonce': int(nonce, 16), 'block': r.get('block')})
